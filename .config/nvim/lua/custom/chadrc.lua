1021961a8d372e51047b5860e52175a3adafef55
Adi De Masi <adi.demasi@gmail.com>
2023-12-19 21:24:28 +0100

    change to radium theme

--- .config/nvim/lua/custom/chadrc.lua
+++ .config/nvim/lua/custom/chadrc.lua
@@ -5,8 +5,8 @@ local M = {}
 local highlights = require "custom.highlights"
 
 M.ui = {
-  theme = "chadracula",
-  theme_toggle = { "chadracula", "one_light" },
+  theme = "radium",
+  theme_toggle = { "radium", "one_light" },
 
   hl_override = highlights.override,
   hl_add = highlights.add,
