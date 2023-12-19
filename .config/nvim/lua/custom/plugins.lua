982c77da3bda476af8d6597e30d0a0257220f11e
Adi De Masi <adi.demasi@gmail.com>
2023-12-19 21:32:09 +0100

    nvim-surround

--- .config/nvim/lua/custom/plugins.lua
+++ .config/nvim/lua/custom/plugins.lua
@@ -124,6 +124,16 @@ local plugins = {
 		end,
 	},
 
+  {
+    "kylechui/nvim-surround",
+    version = "*", -- Use for stability; omit to use `main` branch for the latest features
+    event = "VeryLazy",
+    config = function()
+        require("nvim-surround").setup({
+            -- Configuration here, or leave empty to use defaults
+        })
+    end
+  },
 
   -- To make a plugin not be loaded
   -- {
