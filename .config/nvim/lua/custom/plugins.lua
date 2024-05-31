e7710fe4fc5ecefceb924966ec3e2a384f5fdca7
Adi De Masi <adi.demasi@gmail.com>
2024-05-31 13:23:48 +0200

    new markdown preview plugin definition

--- .config/nvim/lua/custom/plugins.lua
+++ .config/nvim/lua/custom/plugins.lua
@@ -47,6 +47,12 @@ local plugins = {
     end,
   },
   {
+    "iamcco/markdown-preview.nvim",
+    cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+    ft = { "markdown" },
+    build = function() vim.fn["mkdp#util#install"]() end,
+  },
+  {
     "alexghergh/nvim-tmux-navigation",
     lazy = false,
     config = function()
@@ -62,12 +68,6 @@ local plugins = {
         }
       }
     end
-  },
-  {
-    "iamcco/markdown-preview.nvim",
-    build = "cd app && npm install",
-    init = function() vim.g.mkdp_filetypes = { "markdown" } end,
-    ft = { "markdown" },
   },
   {
     "zbirenbaum/copilot.lua",
