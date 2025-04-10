e9636d7bf8489ca8b7fc7404ef147644ca27adf1
Adi De Masi <adi.demasi@gmail.com>
2025-04-10 10:04:23 +0200

    fixing vim config

--- .config/nvim-kickstart/lua/kickstart/plugins/lspconfig.lua
+++ .config/nvim-kickstart/lua/kickstart/plugins/lspconfig.lua
@@ -219,11 +219,9 @@ return {
       -- for you, so that they are available from within Neovim.
       local ensure_installed = vim.tbl_keys(servers or {})
       vim.list_extend(ensure_installed, {
-        'stylua', -- Used to format Lua code
-        'eslint_d',
-        'node-debug2-adapter',
-        'typescript-language-server',
+        'ts_ls',
         'lua_ls',
+        'eslint',
       })
       require('mason-tool-installer').setup { ensure_installed = ensure_installed }
 
